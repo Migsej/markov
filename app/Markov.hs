87b60c1ee4a141703a92e6beddcabc0ab5c0d7be
Migsej <vincentkbonne@gmail.com>
2024-05-15 16:11:41 +0200

    addedexplicit models

--- app/Markov.hs
+++ app/Markov.hs
@@ -4,7 +4,8 @@ import qualified Data.HashMap.Strict as Map
 import Data.Hashable (Hashable)
 import System.Random (Random (random), RandomGen (split))
 
-type Model a num = Map.HashMap a (Map.HashMap a num)
+type Model a = Map.HashMap a (Map.HashMap a Float)
+type FrequencyModel a = Map.HashMap a (Map.HashMap a Int)
 
 pick :: (RandomGen g) => [(a, Float)] -> g -> (g, a)
 pick items seed = (snd (split seed), pick' (fst (random seed)) items)
@@ -15,7 +16,7 @@ pick items seed = (snd (split seed), pick' (fst (random seed)) items)
         | r < probability = x
         | otherwise = pick' (r - probability) xs
 
-generateRandom :: ((RandomGen g), (Hashable a), (Show a)) => g -> a -> Model a Float -> [a]
+generateRandom :: ((RandomGen g), (Hashable a), (Show a)) => g -> a -> Model a -> [a]
 generateRandom rand start model = start : generateRandom new_rand choice model
   where
     possible_states = case Map.lookup start model of
@@ -23,7 +24,7 @@ generateRandom rand start model = start : generateRandom new_rand choice model
         Nothing -> error $ "no outgoing state " ++ show start ++ "in model"
     (new_rand, choice) = pick (Map.toList possible_states) rand
 
-generate :: (Hashable a) => a -> Model a Float -> [a]
+generate :: (Hashable a) => a -> Model a -> [a]
 generate start model = choice : generate choice model
   where
     choice = choose (model Map.! start)
@@ -32,23 +33,23 @@ generate start model = choice : generate choice model
       where
         state_list = Map.toList states
 
-createModel :: (Hashable a) => [a] -> Model a Float
-createModel states = Map.map convert_probability $ addMembers states Map.empty
-  where
-    convert_probability :: (Hashable a) => Map.HashMap a Int -> Map.HashMap a Float
-    convert_probability model = Map.fromList $ map (\(state, x) -> (state, fromIntegral x / total_entries :: Float)) model_list
-      where
-        model_list = Map.toList model
-        total_entries = fromIntegral $ foldl (\acc (_, b) -> acc + b) 0 model_list
+createModel :: (Hashable a) => [a] -> Model a
+createModel states = Map.map convertProbability $ addMembers states Map.empty
 
-combine :: (Hashable a) => Model a Int -> Model a Int -> Model a Int
+convertProbability :: (Hashable a) => Map.HashMap a Int -> Map.HashMap a Float
+convertProbability model = Map.fromList $ map (\(state, x) -> (state, fromIntegral x / total_entries :: Float)) model_list
+  where
+    model_list = Map.toList model
+    total_entries = fromIntegral $ foldl (\acc (_, b) -> acc + b) 0 model_list
+
+combine :: (Hashable a) => FrequencyModel a -> FrequencyModel a -> FrequencyModel a
 combine = Map.unionWith $ Map.unionWith (+)
 
-addMembers :: (Hashable a) => [a] -> Model a Int -> Model a Int
+addMembers :: (Hashable a) => [a] -> FrequencyModel a -> FrequencyModel a
 addMembers (cur : next : rest) model = combine (addMember cur next model) (addMembers (next : rest) model)
 addMembers _ model = model
 
-addMember :: (Hashable a) => a -> a -> Model a Int -> Model a Int
+addMember :: (Hashable a) => a -> a -> FrequencyModel a -> FrequencyModel a
 addMember cur next = Map.insertWith insert cur (Map.singleton next 1)
   where
     insert states = Map.insertWith (+) key val
